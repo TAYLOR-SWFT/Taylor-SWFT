4d34528cba6048b650293d63a7de5c6ca1163106
Marius Rodrigues <marius.rodrigues@telecom-paris.fr>
2026-07-06 14:38:20 +0200

    fix(main): de-comment run_BRAS_eval

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 if __name__ == "__main__":
     data_dir = Path("data/BRAS")
     exp_dir = Path("run/test_new_evaluation")
-    # run_BRAS_eval(data_dir, exp_dir, recompute_metrics=False)
-    # save_statistics(exp_dir)
+    run_BRAS_eval(data_dir, exp_dir, recompute_metrics=False)
+    save_statistics(exp_dir)
     make_table(exp_dir)
     detail_computation_times_on_BRAS_CR3()
